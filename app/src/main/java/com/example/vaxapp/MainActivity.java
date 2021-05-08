5ffb9e935877c1c5bc1669cd68eab2500fe15edd
Vatsal Shreekant <vatsal.shreekant@ryerson.ca>
2021-05-08 13:08:09 -0400

    VaxApp added

---
+++ app/src/main/java/com/example/vaxapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.vaxapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vaxapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
