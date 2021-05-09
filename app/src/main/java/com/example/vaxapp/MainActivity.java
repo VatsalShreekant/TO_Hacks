e7e9c2827981db60256535b291b68c11faa5ec46
Nirahulan <60203633+Nirahulan@users.noreply.github.com>
2021-05-08 20:10:50 -0400

    Registration UI

--- app/src/main/java/com/example/vaxapp/MainActivity.java
+++ app/src/main/java/com/example/vaxapp/MainActivity.java
@@ -1,11 +1,30 @@
 package com.example.vaxapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.vaxapp.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void backButton(View v){
+        Log.i("info:","Back Button Worked");
+            }
+
+    public void nextButton(View v){
+        Log.i("info:","Next Button Worked");
+        Intent intent = new Intent(this, ToActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
